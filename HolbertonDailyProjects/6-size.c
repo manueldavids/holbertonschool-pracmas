b2eb83941adda6455981a346bb1a3e7de8dfd2e3
voltamonkey <9461@holbertonstudents.com>
2024-12-30 02:02:33 -0400

    Day1

---
+++ HolbertonDailyProjects/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(long int));
+	printf("Size of a long long int: %zu\n", sizeof(long long int));
+	printf("Size of a float: %d\n", sizeof(float));
+
+		return 0;
+}
