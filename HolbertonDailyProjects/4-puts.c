93f3060f206076e6f8fa55c3dd301f5476fc8117
voltamonkey <9461@holbertonstudents.com>
2024-12-30 01:28:39 -0400

    First C programming

---
+++ HolbertonDailyProjects/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main void
+ *
+ * Use puts to print a sentence
+ *
+ * Return Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
